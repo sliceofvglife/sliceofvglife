5570f604c0550fc126d2182d30c14897a88f8519
sliceofvglife <jeremy.morosi.dev+sliceofvglife@gmail.com>
2024-11-18 12:54:07 +0100

    empty

--- src/redux/actions/types.ts
+++
@@ -1,4 +0,0 @@
-import { ActionType } from 'typesafe-actions';
-import * as actions from './index';
-
-export type Actions = ActionType<typeof actions>;
--- src/redux/selectors/index.ts
+++
@@ -1,6 +0,0 @@
-import { MenuState } from "../actions";
-
-export const getMenuState = (store: any) => store.menu;
-
-export const getMenu = (store: any): MenuState =>
-    getMenuState(store) ? getMenuState(store) : {};
