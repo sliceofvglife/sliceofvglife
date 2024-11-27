19d4a2eaa79b7d425688eedcbb2e529ae9d7f388
sliceofvglife <jeremy.morosi.dev+sliceofvglife@gmail.com>
2024-11-27 09:33:50 +0100

    clean

--- src/redux/dispatchers/index.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch } from "redux";
-import { Actions } from "../actions/types";
-import { toggleMenu } from "../actions";
-
-export const dispatchToggleMenu =
-    (dispatch: Dispatch<Actions>) => (value: boolean) =>
-        dispatch(toggleMenu(value));
--- src/redux/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import menu from "./menu";
-import { routerReducer } from "react-router-redux";
-
-export default combineReducers({
-    menu,
-    routing: routerReducer
-});
